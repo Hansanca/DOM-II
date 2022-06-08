b513757ac51a8d21334f9258400b9e4094381074
Hansanca <hansanca@gmail.com>
2022-06-08 16:16:07 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
+/* eslint-disable no-undef */
 import './less/index.less'
 
 // Your code goes here!
+
+// load
+window.onload = function (evt) {
+    console.log(`event ${evt.type} test`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Ready'
+
+     // copy
+    window.addEventListener('copy' , () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text
+
+            })
+    })
+
+    //  click
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+
+    // double-click
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.innerHTML = ' '
+    })
+
+    // key-down
+    window.addEventListener('keydown', evt => {
+        if (evt.key == 6) {
+            document.body.innerHTML = ' '
+        }
+    })
+
+    // mouse-move
+    document.body.addEventListener('mousemove', evt => {
+        const { clientX, clientY } = evt
+        // console.log(`mouse is at ${clientX}, ${clientY}`)
+    })
+
+    // mouseenter
+    // mouseleave
+    const destinations = document.querySelectorAll('.destination')
+    for (destination of destinations) {
+        destination.addEventListener('mouseenter', evt => {
+            destination.style.fontWeight = 'bold'
+        })
+        // eslint-disable-next-line no-undef
+        destination.addEventListener('mouseleave', evt =>{
+            setTimeout(() => {
+                destination.style.fontWeight = 'initial'
+            }, 500)
+        })
+    }
+}
